ea7f09259707e9b4c2dcb8533596477fc71d2363
Tyler Kellen <tyler@sleekcode.net>
2014-11-03 11:00:03 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# shared-web-worker
+> A minimal implementation for testing cross-tab communication.
+
+## Setup
+
+1. `npm install`
+2. `npm start`
+3. Open multiple tabs to [http://localhost:8000/index.html](http://localhost:8000/index.html)
+4. fin
---
+++ worker.js
@@ -0,0 +1,12 @@
+var connections = [];
+
+self.addEventListener('connect', function(e) {
+  var port = e.ports[0];
+  connections.push(port);
+  port.addEventListener('message', function(e) {
+    connections.forEach(function(conn) {
+      conn.postMessage(e.data);
+    });
+  }, false);
+  port.start();
+}, false);
